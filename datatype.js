e2931216304df665d2badde2e78a0a925d208ddf
silbi900 <silbi900@gmail.com>
2026-06-08 12:57:03 +0600

    student and datatype done

---
+++ datatype.js
@@ -0,0 +1,11 @@
+let name = "Silvia Rahaman"
+let age = 23
+let isStudent = true
+let salary = null
+let phone
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+console.log(typeof salary)
+console.log(typeof phone)
---
+++ student.js
@@ -0,0 +1,16 @@
+let name = "Silvia Rahaman"
+console.log("Name:", name)
+
+let age = 23
+console.log("Age:", age)
+
+let district = "Dhaka"
+console.log("District:", district)
+
+let isStudent = true
+console.log("Student:", isStudent)
+
+
+
+
+
