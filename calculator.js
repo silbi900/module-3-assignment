950b69c296a944de87a87c8443db8d15995dd079
silbi900 <silbi900@gmail.com>
2026-06-08 13:16:14 +0600

    calculator and result part done

---
+++ calculator.js
@@ -0,0 +1,17 @@
+let num1 = 10
+let num2 = 5
+
+let addition = num1 + num2
+console.log("Addition:", addition)
+
+let subraction = num1 - num2
+console.log("Subraction:", subraction)
+
+let multiplication = num1 * num2
+console.log("Multiplication:", multiplication)
+
+let division = num1 / num2
+console.log("Division:", division)
+
+let modulus = num1 % num2
+console.log("Modulus:", modulus)
---
+++ result.js
@@ -0,0 +1,17 @@
+let marks = 75
+
+if (marks>= 80){
+    console.log("A+")
+}
+else if (marks>=70 && marks<=79){
+    console.log("A")
+}
+else if (marks>=60 && marks<=69){
+    console.log("B")
+}
+else if (marks>=50 && marks<=59){
+    console.log("C")
+}
+else{
+    console.log("Fail")
+}
